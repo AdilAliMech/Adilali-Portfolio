5cf177bc536576ef4e397dbff28d00613f6f1b51
Adil Ali <95779268+AdilAliMech@users.noreply.github.com>
2022-06-21 02:21:01 +0530

    dev red

---
+++ my-project/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css'
+import Tweet from './tweet';
+
+
+
+function App() {
+
+
+  
+
+  
+ 
+
+  return (<div> <Tweet/>
+  <Tweet/>
+  <Tweet/>
+  <Tweet/>
+  
+  
+
+  </div>)
+}
+export default App;
---
+++ my-project/src/tweet.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './App.css'
+function Tweet (){
+    return (
+        <div className = "tweet">
+            <h1> 
+                Twitter 
+            </h1>
+            <p> This is a tweet from jon snow</p>
+            <h3> Number of likes</h3>
+        </div>
+    )
+}
+export default Tweet
